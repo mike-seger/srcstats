b25f7a5bf46dc0c1e429b0cfb41f2117195e59fc
mike <ms@n128.com>
2024-06-15 22:28:17 +0000

    ...

--- srcstats.sh
+++ srcstats.sh
@@ -24,9 +24,9 @@ ROOT_DIRS=("$@")
 include_conditions=""
 for pattern in "${START_PATTERNS[@]}"; do
     if [ -z "$include_conditions" ]; then
-        include_conditions="-path \"${pattern}*\""
+        include_conditions="-path \"$pattern*\""
     else
-        include_conditions="$include_conditions -o -path \"${pattern}*\""
+        include_conditions="$include_conditions -o -path \"$pattern*\""
     fi
 done
 
@@ -34,9 +34,9 @@ done
 exclude_conditions=""
 for pattern in "${EXCLUDE_PATTERNS[@]}"; do
     if [ -z "$exclude_conditions" ]; then
-        exclude_conditions="! -path \"$pattern\""
+        exclude_conditions="! -path \"$pattern*\""
     else
-        exclude_conditions="$exclude_conditions -o ! -path \"$pattern\""
+        exclude_conditions="$exclude_conditions -o ! -path \"$pattern*\""
     fi
 done
 
@@ -45,11 +45,22 @@ find_command="find ${ROOT_DIRS[@]} -type f \\( $include_conditions \\) \\( $excl
 
 # Temporary file to store intermediate results
 temp_file=$(mktemp)
-consolidated_file=$(mktemp)
 
 # Debug function
 debug() {
     echo "$@" >&2
+}
+
+# Function to find the longest matching pattern
+longest_matching_pattern() {
+    local path="$1"
+    local longest_match=""
+    for pattern in "${START_PATTERNS[@]}"; do
+        if [[ "$path" == *"$pattern"* ]] && [[ ${#pattern} -gt ${#longest_match} ]]; then
+            longest_match="$pattern"
+        fi
+    done
+    echo "$longest_match"
 }
 
 # Process directories
@@ -57,41 +68,11 @@ debug "finding files: $find_command"
 
 eval $find_command | while read -r file; do
     lines_in_file=$(count_lines_matching_pattern "$file")
-    echo -e "${file}\t${lines_in_file}" >> "$temp_file"
+    longest_match=$(longest_matching_pattern "$file")
+    echo -e "${file}\t${longest_match}\t${lines_in_file}" >> "$temp_file"
 done
 
-# Consolidate results for matching directories
-debug "Consolidating results..."
-awk -F'\t' '{files[$1] += 1; lines[$1] += $2} END {for (dir in files) print dir, files[dir], lines[dir]}' OFS='\t' "$temp_file" > "$consolidated_file"
-
-cp "$consolidated_file" /tmp/1
-
-# Aggregate results with specified depth and categories
-debug "Aggregating final results with max depth $MAX_DEPTH..."
-echo -e "path\tfiles\tlines"
-awk -v max_depth="$MAX_DEPTH" -F'\t' '
-function trim_path(path, depth) {
-    split(path, parts, "/")
-    if (length(parts) <= depth) {
-        return path
-    }
-    result = parts[1]
-    for (i = 2; i <= depth; i++) {
-        result = result "/" parts[i]
-    }
-    return result "/..."
-}
-{
-    dir = trim_path($1, max_depth)
-    files[dir] += $2
-    lines[dir] += $3
-}
-END {
-    for (dir in files) {
-        print dir "\t" files[dir] "\t" lines[dir]
-    }
-}
-' "$consolidated_file" | sed 's/\/\.\.\.\//\//g' | sort
+cat "$temp_file"
 
 # Cleanup
-rm "$temp_file" "$consolidated_file"
+rm "$temp_file"
